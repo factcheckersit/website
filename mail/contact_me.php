94f657ef264525aeecc656a0adb5162b9c517b3b
nicolabruno <nicobruno@gmail.com>
2017-02-08 12:22:22 +0100

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,23 +1,23 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
+if(empty($_POST['nome'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['telefono']) 		||
+   empty($_POST['messaggio'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 	
-$name = strip_tags(htmlspecialchars($_POST['name']));
+$name = strip_tags(htmlspecialchars($_POST['nome']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$phone = strip_tags(htmlspecialchars($_POST['telefono']));
+$message = strip_tags(htmlspecialchars($_POST['messaggio']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'factcheckers.it@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Factcheckers Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
