5174543bebaa1ee787d903267cf86301e3b29f37
nicolabruno <nicobruno@gmail.com>
2017-02-08 12:25:13 +0100

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,19 +1,19 @@
 <?php
 // Check for empty fields
-if(empty($_POST['nome'])  		||
+if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['telefono']) 		||
-   empty($_POST['messaggio'])	||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 	
-$name = strip_tags(htmlspecialchars($_POST['nome']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['telefono']));
-$message = strip_tags(htmlspecialchars($_POST['messaggio']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'factcheckers.it@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
